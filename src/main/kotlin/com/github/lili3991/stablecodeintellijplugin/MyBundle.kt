267743d187018a79080da3e2f87aec4c87675f20
GitHub Action <action@github.com>
2025-12-14 06:47:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/lili3991/stablecodeintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/lili3991/stablecodeintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lili3991.stablecodeintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lili3991/stablecodeintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lili3991/stablecodeintellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.lili3991.stablecodeintellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lili3991.stablecodeintellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
